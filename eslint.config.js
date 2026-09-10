b7f003e06448568f861ab101f54abfa75223d10e
vallo7 <vallotsopna@gmail.com>
2026-09-10 14:41:20 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,12 @@ export default [
       "dist/**",
       "android/**"
     ],
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        window: "readonly"
+      }
+    },
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error"
